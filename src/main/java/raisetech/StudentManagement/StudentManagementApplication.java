3d8900921296de83408aded12bfc066ea27ba56e
take-kubo <takubokawa@gmail.com>
2025-09-20 04:42:53 +0900

    feat: 受講生情報（名前、年齢）を表示・更新する機能に変更
    講座18の動画の内容を再現しました。
    /studentInfoページにGETとPOSTをできるようにしました。POSTのパラメータはnameとageです。
    /studentNameページにPOSTできるようにしました。POSTのパラメータはnameだけです。

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -3,19 +3,35 @@ package raisetech.StudentManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private String name = "Yamada Taro";
+	private String age = "40";
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello World!";
+	/* 課題：講座の再現をする */
+	@GetMapping("/studentInfo")
+	public String getStudentInfo() {
+		return name + " " + age + "歳";
+	}
+
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name, String age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@PostMapping("/studentName")
+	public void updateStudentName(String name) {
+		this.name = name;
 	}
 
 }
