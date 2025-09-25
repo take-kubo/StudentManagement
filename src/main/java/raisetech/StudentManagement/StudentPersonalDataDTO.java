b9a1861c5b3ac7c9f8df582f5aa5c967176874da
take-kubo <takubokawa@gmail.com>
2025-09-26 04:55:48 +0900

    [clean] StudentPersonalDataDTO クラスにコメントを追加
    クラスの役割を説明するためにコメントを追加しました。

--- src/main/java/raisetech/StudentManagement/StudentPersonalDataDTO.java
+++ src/main/java/raisetech/StudentManagement/StudentPersonalDataDTO.java
@@ -1,10 +1,19 @@
 package raisetech.StudentManagement;
 
+/*
+  リクエストのBodyで渡された JSON データを受け取るための DTO(Data Transfer Object) クラス
+
+  コントローラとサービスの間で受講生情報をやり取りするために使用します。
+ */
 public class StudentPersonalDataDTO {
 
+  // 受講生の名前
   private String studentName;
+
+  // 受講生の年齢
   private String studentAge;
 
+  // コンストラクタ
   public StudentPersonalDataDTO() {
     studentName = "unknown";
     studentAge = "unknown";
