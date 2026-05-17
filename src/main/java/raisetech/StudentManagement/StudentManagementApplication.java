bbe6d57e68760a9a11da6d8456de6882a34867ab
take-kubo <takubokawa@gmail.com>
2026-05-18 06:13:38 +0900

    [refactor]不要なインポートを削除
    ・MapとHashMapのimport文を削除しました
    ・理由：前回のコミットでマップstudentを削除したがインポート文が残っていたから

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,7 +1,5 @@
 package raisetech.StudentManagement;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
