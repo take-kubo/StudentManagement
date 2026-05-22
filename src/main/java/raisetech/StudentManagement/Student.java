f080a2e5d0f3a4084a9d2d32afd0ffb6f4089f35
take-kubo <takubokawa@gmail.com>
2026-05-23 05:38:39 +0900

    [change]データオブジェクトを新しいテーブルに合わせて変更
    ・受講生情報のデータオブジェクトに新しいフィールドを追加しました
    ・追加したのは、id, furigana, nickname, email, address, age, genderです
    ・理由：接続するテーブルを新しいテーブルに変更したから

--- src/main/java/raisetech/StudentManagement/Student.java
+++ src/main/java/raisetech/StudentManagement/Student.java
@@ -2,22 +2,13 @@ package raisetech.StudentManagement;
 
 public class Student {
 
+  private String id;
   private String name;
+  private String furigana;
+  private String nickname;
+  private String email;
+  private String address;
   private int age;
+  private String gender;
 
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
 }
