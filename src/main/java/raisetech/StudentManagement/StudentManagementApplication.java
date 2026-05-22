85d78fb8c94d0bdc103a9f2c1de11349b7dd6f79
take-kubo <takubokawa@gmail.com>
2026-05-22 20:13:30 +0900

    [change]登録・更新・削除機能を削除
    ・registerStudent、updateStudent、deleteStudentを削除しました
    ・理由：講義23でREAD処理を実装する準備です

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -3,10 +3,7 @@ package raisetech.StudentManagement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,21 +22,6 @@ public class StudentManagementApplication {
   public String getStudent(@RequestParam String name) {
     Student student = repository.searchByName(name);
     return student.getName() + " " + student.getAge() + "歳";
-  }
-
-  @PostMapping("/student")
-  public void registerStudent(@RequestParam String name, @RequestParam int age) {
-    repository.registerStudent(name, age);
-  }
-
-  @PatchMapping("/student")
-  public void updateStudent(@RequestParam String name, @RequestParam int age) {
-    repository.updateStudent(name, age);
-  }
-
-  @DeleteMapping("/student")
-  public void deleteStudent(@RequestParam String name) {
-    repository.deleteStudent(name);
   }
 
 }
