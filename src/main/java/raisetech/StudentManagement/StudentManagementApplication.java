fbb8f97a2b573278994260e422e48bfb33fea896
take-kubo <takubokawa@gmail.com>
2026-05-25 19:36:00 +0900

    [add]UI層を作成
    ・3層レイヤー構成のUI層としてcontrollerパッケージを作成しました
    ・そこにStudentControllerを作成しました
    ・クラスに@RestControllerアノテーションを付与しました
    ・理由：3層レイヤー構成にするため

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -5,13 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 import raisetech.StudentManagement.repository.StudentRepository;
 
 @SpringBootApplication
-@RestController
 public class StudentManagementApplication {
 
   @Autowired
