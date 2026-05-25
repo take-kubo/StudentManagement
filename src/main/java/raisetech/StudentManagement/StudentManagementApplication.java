97efa6bd1a4d38b879ec8d4b4f76b6c7d28e620c
take-kubo <takubokawa@gmail.com>
2026-05-25 19:43:01 +0900

    [add]コントローラにコンストラクタと取得メソッドを実装
    ・コントローラにコンストラクタを作りました
    ・@Autowiredアノテーションを付与しStudentServiceをインジェクションしました
    ・StudentManagementApplicationにある取得メソッドをコントローラに移動しました

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,31 +1,13 @@
 package raisetech.StudentManagement;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import raisetech.StudentManagement.data.Student;
-import raisetech.StudentManagement.data.StudentsCourses;
-import raisetech.StudentManagement.repository.StudentRepository;
 
 @SpringBootApplication
 public class StudentManagementApplication {
-
-  @Autowired
-  StudentRepository repository;
 
   public static void main(String[] args) {
     SpringApplication.run(StudentManagementApplication.class, args);
   }
 
-  @GetMapping("/studentList")
-  public List<Student> getStudentList() {
-    return repository.searchStudents();
-  }
-
-  @GetMapping("/studentsCourseList")
-  public List<StudentsCourses> getStudentsCourseList() {
-    return repository.searchStudentsCourses();
-  }
 }
