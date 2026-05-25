cbcca092090a2eb0e6fb570960f8cf2dbd21d57e
take-kubo <takubokawa@gmail.com>
2026-05-25 19:31:32 +0900

    [add]サービスにコンストラクタとサーチメソッドを実装
    ・サービスにコンストラクタを実装しました
    ・コンストラクタにはAutowiredアノテーションを付与しStudentRepositoryをインジェクションしています
    ・受講生情報を全件取得して返すメソッドを実装しました
    ・受講生コース情報を全件取得して返すメソッドを実装しました

--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -1,8 +1,27 @@
 package raisetech.StudentManagement.service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.repository.StudentRepository;
 
 @Service
 public class StudentService {
-  
+
+  private final StudentRepository repository;
+
+  @Autowired
+  public StudentService(StudentRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Student> searchStudentList() {
+    return repository.searchStudents();
+  }
+
+  public List<StudentsCourses> searchStudentsCourseList() {
+    return repository.searchStudentsCourses();
+  }
 }
