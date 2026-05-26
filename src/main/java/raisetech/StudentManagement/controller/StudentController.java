f862e6cdf1e9c2ffda97a67df6b1a7d1951fb727
take-kubo <takubokawa@gmail.com>
2026-05-27 05:10:36 +0900

    [add]値の詰替え機能をコントローラに追加
    ・StudentとStudentCoursesからStudentDetailsを生成するコードを記述しました
    ・記述場所はgetStudentListです
    ・理由：画面で必要な情報なので、まずはコントローラに実装することになったから

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -1,11 +1,14 @@
 package raisetech.StudentManagement.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
 @RestController
@@ -19,8 +22,23 @@ public class StudentController {
   }
 
   @GetMapping("/studentList")
-  public List<Student> getStudentList() {
-    return service.searchStudentList();
+  public List<StudentDetail> getStudentList() {
+    List<Student> students = service.searchStudentList();
+    List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
+
+    List<StudentDetail> studentDetails = new ArrayList<>();
+    students.forEach(student -> {
+      StudentDetail studentDetail = new StudentDetail();
+      studentDetail.setStudent(student);
+      
+      List<StudentsCourses> convertStudentCourses = studentsCourses.stream()
+          .filter(studentCourse -> student.getId().equals(studentCourse.getStudentId()))
+          .collect(Collectors.toList());
+      studentDetail.setStudentsCourses(convertStudentCourses);
+      studentDetails.add(studentDetail);
+
+    });
+    return studentDetails;
   }
 
   @GetMapping("/studentsCourseList")
