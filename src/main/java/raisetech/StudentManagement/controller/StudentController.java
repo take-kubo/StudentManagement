1adca51953857df7942e9d06eb17bd064757dd25
take-kubo <takubokawa@gmail.com>
2026-05-27 05:28:38 +0900

    [ref]値の詰替え機能をコンバータに移動
    ・controllerパッケージの中にconverterパッケージを作り、コンバータとしてStudentConverterを配置しました
    ・StudentControllerに作った値詰替機能をStudentConverterに移動しました
    ・これに合わせて、コントローラのフィールドとしてStudentConverterを登録し、コンストラクタでインジェクションしました
    ・StuderntCoverterに@Componentアノテーションを付与しBean登録しました
    ・理由：値詰替機能が複雑なためコンバータとして独立させたかったからです

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -1,11 +1,10 @@
 package raisetech.StudentManagement.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 import raisetech.StudentManagement.domain.StudentDetail;
@@ -15,10 +14,12 @@ import raisetech.StudentManagement.service.StudentService;
 public class StudentController {
 
   private final StudentService service;
+  private final StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service) {
+  public StudentController(StudentService service, StudentConverter converter) {
     this.service = service;
+    this.converter = converter;
   }
 
   @GetMapping("/studentList")
@@ -26,19 +27,7 @@ public class StudentController {
     List<Student> students = service.searchStudentList();
     List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
 
-    List<StudentDetail> studentDetails = new ArrayList<>();
-    students.forEach(student -> {
-      StudentDetail studentDetail = new StudentDetail();
-      studentDetail.setStudent(student);
-      
-      List<StudentsCourses> convertStudentCourses = studentsCourses.stream()
-          .filter(studentCourse -> student.getId().equals(studentCourse.getStudentId()))
-          .collect(Collectors.toList());
-      studentDetail.setStudentsCourses(convertStudentCourses);
-      studentDetails.add(studentDetail);
-
-    });
-    return studentDetails;
+    return converter.convertStudentDetails(students, studentsCourses);
   }
 
   @GetMapping("/studentsCourseList")
