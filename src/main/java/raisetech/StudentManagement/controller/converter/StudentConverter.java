6e3829b9dcb42626a3b4427b3f7ef290b9d81b40
take-kubo <takubokawa@gmail.com>
2026-05-29 06:00:20 +0900

    [fix] 受講生情報のIDに対してnullチェックをコンバータに実装
    ・student.getId()がnullを返す場合、アプリを終了するようにしました
    ・コンバータはStream APIで書かれていましたが、for文に書き換えました。
    ・理由：万が一null担った場合、アプリが強制終了してしまうからです

--- src/main/java/raisetech/StudentManagement/controller/converter/StudentConverter.java
+++ src/main/java/raisetech/StudentManagement/controller/converter/StudentConverter.java
@@ -2,30 +2,50 @@ package raisetech.StudentManagement.controller.converter;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 import raisetech.StudentManagement.domain.StudentDetail;
 
+@Slf4j
 @Component
 public class StudentConverter {
 
   public List<StudentDetail> convertStudentDetails(List<Student> students,
       List<StudentsCourses> studentsCourses) {
+
     List<StudentDetail> studentDetails = new ArrayList<>();
-    students.forEach(student -> {
+
+    for (Student student : students) {
       StudentDetail studentDetail = new StudentDetail();
       studentDetail.setStudent(student);
 
-      List<StudentsCourses> convertStudentCourses = studentsCourses.stream()
-          .filter(studentCourse -> student.getId().equals(studentCourse.getStudentId()))
-          .collect(Collectors.toList());
+      // 受講生情報のID（student.getId()）がnullでないかチェックしています
+      try {
+        Objects.requireNonNull(student.getId());
+      } catch (NullPointerException error) {
+        log.error(
+            "The id field of student is null. The students table in studentmanagement database includes null id.",
+            error);
+        System.exit(1);
+      }
+
+      List<StudentsCourses> convertStudentCourses = new ArrayList<>();
+      for (StudentsCourses studentCourse : studentsCourses) {
+
+        if (student.getId().equals(studentCourse.getStudentId())) {
+          convertStudentCourses.add(studentCourse);
+        }
+      }
+
       studentDetail.setStudentsCourses(convertStudentCourses);
       studentDetails.add(studentDetail);
+    }
 
-    });
     return studentDetails;
+
   }
 
 }
