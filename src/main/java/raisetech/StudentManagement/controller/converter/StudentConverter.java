b3c16db0f4f294c4c98c2a6f178399775243267a
take-kubo <takubokawa@gmail.com>
2026-06-01 18:55:39 +0900

    [change]講義27開始の状態に復元
    ・GlobalExceptionHandlerを削除しました
    ・Exceptionパッケージを削除しました
    ・StudentConverterの例外処理を削除しました
    ・StudentConverterの２重forループをストリームAPIに置き換えました
    ・StudentConverterのJavaDocを削除しました
    ・理由：講義27の内容を再現するためです

--- src/main/java/raisetech/StudentManagement/controller/converter/StudentConverter.java
+++ src/main/java/raisetech/StudentManagement/controller/converter/StudentConverter.java
@@ -2,76 +2,31 @@ package raisetech.StudentManagement.controller.converter;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import lombok.extern.slf4j.Slf4j;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 import raisetech.StudentManagement.domain.StudentDetail;
 
-/**
- * コントローラ用にデータの詰め替えを担当するコンバータ。
- */
-@Slf4j
 @Component
 public class StudentConverter {
 
-  /**
-   * 受講生詳細情報のリストを生成する。
-   *
-   * <p>受講生情報と受講生コース情報を結合して受講生詳細情報を生成します。
-   * 受講生情報のIDがnullの場合NPEを投げます。
-   * 受講生コース情報が存在しない場合、空の受講生コース情報を使って受講生詳細情報を生成します。
-   * どちらの場合でもエラーログを出力します。</p>
-   *
-   * @param students 受講生情報のリスト
-   * @param studentsCourses 受講生コース情報のリスト
-   * @return 受講生詳細情報のリスト
-   * @throws NullPointerException 受講生情報のIDがnullの場合
-   */
   public List<StudentDetail> convertStudentDetails(List<Student> students,
-      List<StudentsCourses> studentsCourses) throws NullPointerException {
-
-    // 受講生コース情報のリストがnullでないかチェックしています
-    // nullだった場合は初期化された受講生コース情報をひとつ持つリストにしています
-    try {
-      Objects.requireNonNull(studentsCourses);
-    } catch (NullPointerException error) {
-      log.error(
-          "The studentsCourses list is null.",
-          error);
-      StudentsCourses emptyStudentsCourse = new StudentsCourses();
-      studentsCourses = List.of(emptyStudentsCourse);
-    }
+      List<StudentsCourses> studentsCourses) {
 
     List<StudentDetail> studentDetails = new ArrayList<>();
 
-    for (Student student : students) {
+    students.forEach(student -> {
       StudentDetail studentDetail = new StudentDetail();
       studentDetail.setStudent(student);
 
-      // 受講生情報のID（student.getId()）がnullでないかチェックしています
-      // nullの場合はNPEを投げます
-      try {
-        Objects.requireNonNull(student.getId());
-      } catch (NullPointerException error) {
-        log.error(
-            "The id field of student is null. The students table in studentmanagement database includes null id.",
-            error);
-        throw new NullPointerException();
-      }
-
-      List<StudentsCourses> convertStudentCourses = new ArrayList<>();
-      for (StudentsCourses studentCourse : studentsCourses) {
-
-        if (student.getId().equals(studentCourse.getStudentId())) {
-          convertStudentCourses.add(studentCourse);
-        }
-      }
+      List<StudentsCourses> convertStudentCourses = studentsCourses.stream()
+          .filter(studentCourse -> student.getId().equals(studentCourse.getStudentId()))
+          .collect(Collectors.toList());
 
       studentDetail.setStudentsCourses(convertStudentCourses);
       studentDetails.add(studentDetail);
-    }
+    });
 
     return studentDetails;
 
