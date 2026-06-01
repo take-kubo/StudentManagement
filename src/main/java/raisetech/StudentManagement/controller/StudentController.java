c0258fdf630a90fa510cbae9b75c908071378cbf
take-kubo <takubokawa@gmail.com>
2026-06-01 20:22:08 +0900

    [add]Thymeleafを使った描画を実装
    ・RestControllerアノテーションをControllerアノテーションに変更しました
    ・エンドポイントstudentListで返す形をStringに変更しました。
    ・model.addAttributeでhtmlファイル内のstudentListをコンバータの返り値StudentDetailのリストに紐づけました
    ・returnするのはhtmlファイル名です

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -2,15 +2,15 @@ package raisetech.StudentManagement.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
-import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
-@RestController
+@Controller
 public class StudentController {
 
   private final StudentService service;
@@ -23,11 +23,12 @@ public class StudentController {
   }
 
   @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList() {
+  public String getStudentList(Model model) {
     List<Student> students = service.searchStudentList();
     List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
 
-    return converter.convertStudentDetails(students, studentsCourses);
+    model.addAttribute("studentList", converter.convertStudentDetails(students, studentsCourses));
+    return "studentList";
   }
 
   @GetMapping("/studentsCourseList")
