2cfa5d2a4586a9da37019854e9be9758839c9697
take-kubo <takubokawa@gmail.com>
2026-06-07 07:20:22 +0900

    [add]受講生情報を登録するPOSTメソッド作成
    ・registerStudentにPOSTするとフォームに入力したデータが送られます
    ・registerStudentメソッドの引数にModelAttributeアノテーションをつけてデータを受け取っています
    ・フォームのデータと引数の紐づけ結果をBindingResultで受け取っています
    ・紐づけが失敗した場合、登録画面を表示します
    ・紐づけが成功した場合、一覧表示画面にリダイレクトします

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -4,7 +4,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
@@ -41,5 +44,14 @@ public class StudentController {
   public String newStudent(Model model) {
     model.addAttribute("studentDetail", new StudentDetail());
     return "registerStudent";
+  }
+
+  @PostMapping("/registerStudent")
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+    if (result.hasErrors()) {
+      return "registerStudent";
+    }
+
+    return "redirect:/studentList";
   }
 }
