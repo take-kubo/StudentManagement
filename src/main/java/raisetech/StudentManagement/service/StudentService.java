ab029559dd52cbb2801831c7487d1ef59720adcd
take-kubo <takubokawa@gmail.com>
2026-06-09 06:11:05 +0900

    [add]受講生情報の受講生IDと受講生コース情報の受講生IDを紐づけ
    ・studentsCoursesのidにstudentのidを代入しました

--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -1,6 +1,7 @@
 package raisetech.StudentManagement.service;
 
 import java.util.List;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import raisetech.StudentManagement.data.Student;
@@ -34,6 +35,9 @@ public class StudentService {
     // 受講生コース情報登録用のUUID生成
     String studentsCoursesUuid = UUID.randomUUID().toString();
     studentsCourses.setId(studentsCoursesUuid);
+
+    // 受講生のIDを受講生コース情報に代入
+    studentsCourses.setStudentId(student.getId());
 
     // リポジトリを呼び出してデータベースに登録
     repository.registerStudent(student);
