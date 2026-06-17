da39069ceb0e2c9a60e3e342b40103131f3a18df
take-kubo <takubokawa@gmail.com>
2026-06-17 19:13:48 +0900

    [change]UUIDをデータベース側で生成するように変更
    ・UUIDを生成しているコードを削除しました
    ・データベース側で生成したUUIDを取得するためにリポジトリにSelectKeyアノテーションを付与しました
    ・受講生IDを受講生コース情報に渡す前にリポジトリのregisterStudentを呼び出しました
    ・理由：registerStudentを呼び出すときにデータベースからUUIDを取得するから

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -3,7 +3,9 @@ package raisetech.StudentManagement.repository;
 import java.util.List;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 
@@ -18,12 +20,26 @@ public interface StudentRepository {
 
   @Insert({
       "INSERT INTO students(id, name, furigana, nickname, email, address, age, gender, remark, is_deleted)",
-      "VALUES(#{id}, #{name}, #{furigana}, #{nickname}, #{email}, #{address}, #{age}, #{gender}, #{remark}, false)"})
-  void registerStudent(Student student);
+      "VALUES(#{student.id}, #{student.name}, #{student.furigana}, #{student.nickname}, #{student.email}, "
+          + "#{student.address}, #{student.age}, #{student.gender}, #{student.remark}, false)"})
+  @SelectKey(
+      statement = "SELECT UUID()",
+      keyProperty = "student.id",
+      before = true,
+      resultType = String.class
+  )
+  void registerStudent(@Param("student") Student student);
 
   @Insert({
       "INSERT INTO students_courses(id, student_id, course_name, course_start_at, course_end_at)",
-      "VALUES(#{id}, #{studentId}, #{courseName}, #{courseStartAt}, #{courseEndAt})"})
-  void registerStudentCourse(StudentsCourses studentsCourses);
+      "VALUES(#{studentsCourses.id}, #{studentsCourses.studentId}, #{studentsCourses.courseName},"
+          + " #{studentsCourses.courseStartAt}, #{studentsCourses.courseEndAt})"})
+  @SelectKey(
+      statement = "SELECT UUID()",
+      keyProperty = "studentsCourses.id",
+      before = true,
+      resultType = String.class
+  )
+  void registerStudentCourse(@Param("studentsCourses") StudentsCourses studentsCourses);
 
 }
