4d3118b362bd83bb7f6ab411e9a5b998038329f8
take-kubo <takubokawa@gmail.com>
2026-07-04 03:54:06 +0900

    [add]入力チェックを通過しない場合の例外処理を追加
    ・データオブジェクトで行われる入力チェックで弾かれた場合の例外処理をGlobalExceptionHandlerクラスに追加しました
    ・理由：デフォルトの例外処理を避け、レスポンスの形式を統一するためです

--- src/main/java/raisetech/StudentManagement/exception/GlobalExceptionHandler.java
+++ src/main/java/raisetech/StudentManagement/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package raisetech.StudentManagement.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,6 +13,13 @@ public class GlobalExceptionHandler {
     return ResponseEntity
         .internalServerError()
         .body("システムエラーが発生しました。また再度アクセスしてください。");
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<String> methodArgumentNotValidExceptionHandler() {
+    return ResponseEntity
+        .badRequest()
+        .body("不正なリクエストです。入力チェックを通過しませんでした。");
   }
 
 }
