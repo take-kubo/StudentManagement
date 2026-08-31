5c9612f5c150c6a9c039a2736411c89eeebb5700
take-kubo <takubokawa@gmail.com>
2026-09-01 05:24:11 +0900

    [test]受講生の更新機能のテストを実装
    ・StudentServiceクラスのupdateStudentメソッドのテストを実装しました
    ・updateStudentメソッド内で呼び出している各メソッドの呼び出し回数を確認しています
    ・呼び出しているメソッドはrepository.updateStudent、repository.updateStudentCourseです
    ・存在しないIDを指定したときの例外テストを実装しました

--- src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
+++ src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
@@ -145,4 +145,103 @@ class StudentServiceTest {
     Mockito.verify(repository, Mockito.times(1)).registerStudentCourse(studentCourseAWS);
   }
 
+  @Test
+  void 受講生の更新_リポジトリの処理が適切に呼び出せていること() {
+    // 準備
+    Student studentInDB = new Student();
+    studentInDB.setId("791ea084-7d68-11f1-be12-84a93e79e4f1");
+    studentInDB.setName("鈴木一郎");
+    studentInDB.setFurigana("スズキイチロウ");
+    studentInDB.setNickname("イチ");
+    studentInDB.setAge(20);
+    studentInDB.setAddress("長野県");
+    studentInDB.setEmail("ichi@test.com");
+    studentInDB.setGender("男");
+    studentInDB.setRemark("特になし");
+    studentInDB.setDeleted(false);
+
+    Student student = new Student();
+    student.setId("791ea084-7d68-11f1-be12-84a93e79e4f1");
+    student.setName("山田太郎");
+    student.setFurigana("ヤマダタロウ");
+    student.setNickname("タロウ");
+    student.setAge(36);
+    student.setAddress("東京都");
+    student.setEmail("taro@test.com");
+    student.setGender("男");
+    student.setRemark("特になし");
+    student.setDeleted(false);
+
+    StudentCourse studentCourseJava = new StudentCourse();
+    studentCourseJava.setStudentId("791ea084-7d68-11f1-be12-84a93e79e4f1");
+    studentCourseJava.setCourseName("Javaコース");
+    studentCourseJava.setCourseStartAt(LocalDateTime.now());
+    studentCourseJava.setCourseEndAt(LocalDateTime.now());
+
+    StudentCourse studentCourseAWS = new StudentCourse();
+    studentCourseAWS.setStudentId("791ea084-7d68-11f1-be12-84a93e79e4f1");
+    studentCourseAWS.setCourseName("AWSコース");
+    studentCourseAWS.setCourseStartAt(LocalDateTime.now());
+    studentCourseAWS.setCourseEndAt(LocalDateTime.now());
+
+    List<StudentCourse> studentCourseList = List.of(studentCourseJava, studentCourseAWS);
+
+    StudentDetail studentDetail = new StudentDetail();
+    studentDetail.setStudent(student);
+    studentDetail.setStudentsCourses(studentCourseList);
+
+    Mockito.when(repository.searchStudent("791ea084-7d68-11f1-be12-84a93e79e4f1"))
+        .thenReturn(studentInDB);
+
+    // 実行
+    sut.updateStudent("791ea084-7d68-11f1-be12-84a93e79e4f1", studentDetail);
+
+    // 検証
+    Mockito.verify(repository, Mockito.times(1))
+        .searchStudent("791ea084-7d68-11f1-be12-84a93e79e4f1");
+    Mockito.verify(repository, Mockito.times(1)).updateStudent(student);
+    Mockito.verify(repository, Mockito.times(1)).updateStudentCourse(studentCourseJava);
+    Mockito.verify(repository, Mockito.times(1)).updateStudentCourse(studentCourseAWS);
+
+  }
+
+  @Test
+  void 受講生の更新_存在しないIDで検索をしたとき適切に例外を投げること() {
+    // 事前準備
+    Student student = new Student();
+    student.setId("791ea084-7d68-11f1-be12-84a93e79e4f1");
+    student.setName("山田太郎");
+    student.setFurigana("ヤマダタロウ");
+    student.setNickname("タロウ");
+    student.setAge(36);
+    student.setAddress("東京都");
+    student.setEmail("taro@test.com");
+    student.setGender("男");
+    student.setRemark("特になし");
+    student.setDeleted(false);
+
+    StudentCourse studentCourseJava = new StudentCourse();
+    studentCourseJava.setStudentId("791ea084-7d68-11f1-be12-84a93e79e4f1");
+    studentCourseJava.setCourseName("Javaコース");
+    studentCourseJava.setCourseStartAt(LocalDateTime.now());
+    studentCourseJava.setCourseEndAt(LocalDateTime.now());
+
+    StudentCourse studentCourseAWS = new StudentCourse();
+    studentCourseAWS.setStudentId("791ea084-7d68-11f1-be12-84a93e79e4f1");
+    studentCourseAWS.setCourseName("AWSコース");
+    studentCourseAWS.setCourseStartAt(LocalDateTime.now());
+    studentCourseAWS.setCourseEndAt(LocalDateTime.now());
+
+    List<StudentCourse> studentCourseList = List.of(studentCourseJava, studentCourseAWS);
+
+    StudentDetail studentDetail = new StudentDetail();
+    studentDetail.setStudent(student);
+    studentDetail.setStudentsCourses(studentCourseList);
+
+    Mockito.when(repository.searchStudent("791ea084-7d68-11f1-be12-84a93e79e4f1")).thenReturn(null);
+
+    // 検証
+    Assertions.assertThrows(StudentNotFoundException.class,
+        () -> sut.updateStudent("791ea084-7d68-11f1-be12-84a93e79e4f1", studentDetail));
+  }
 }
