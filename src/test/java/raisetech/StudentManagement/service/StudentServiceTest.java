e3bd4c9e173db6ffc531f09a62f91e88bbcffa18
take-kubo <takubokawa@gmail.com>
2026-09-02 05:58:59 +0900

    [test]受講生詳細一覧検索の返り値を検証するテストを追記
    ・期待する返り値（受講生詳細のリスト）を設定しました
    ・Assertions.assertEqualsで期待する値と実際の値を比較するようにしました

--- src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
+++ src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
@@ -35,13 +35,77 @@ class StudentServiceTest {
 
   @Test
   void 受講生詳細の一覧検索_リポジトリとコンバーターの処理が適切に呼び出せていること() {
+    // 事前準備
+    Student student1 = new Student();
+    student1.setId("1");
+    student1.setName("テスト太郎");
+    student1.setFurigana("テストタロウ");
+    student1.setNickname("テスタ");
+    student1.setAge(40);
+    student1.setAddress("東京都");
+    student1.setEmail("testa@test.com");
+    student1.setGender("男");
+    student1.setRemark("特になし");
+    student1.setDeleted(false);
+
+    Student student2 = new Student();
+    student2.setId("2");
+    student2.setName("テスト次郎");
+    student2.setFurigana("テストジロウ");
+    student2.setNickname("テスジ");
+    student2.setAge(40);
+    student2.setAddress("神奈川県");
+    student2.setEmail("tesji@test.com");
+    student2.setGender("男");
+    student2.setRemark("特になし");
+    student2.setDeleted(false);
+
     List<Student> studentList = new ArrayList<>();
-    List<StudentCourse> studentCourseList = new ArrayList<>();
+    studentList.add(student1);
+    studentList.add(student2);
     Mockito.when(repository.searchStudentList()).thenReturn(studentList);
+
+    StudentCourse studentCourseJava = new StudentCourse();
+    studentCourseJava.setId("1");
+    studentCourseJava.setStudentId("1");
+    studentCourseJava.setCourseName("Javaコース");
+    studentCourseJava.setCourseStartAt(LocalDateTime.now());
+    studentCourseJava.setCourseEndAt(LocalDateTime.now());
+
+    StudentCourse studentCourseAWS = new StudentCourse();
+    studentCourseAWS.setId("2");
+    studentCourseAWS.setStudentId("2");
+    studentCourseAWS.setCourseName("AWSコース");
+    studentCourseAWS.setCourseStartAt(LocalDateTime.now());
+    studentCourseAWS.setCourseEndAt(LocalDateTime.now());
+
+    List<StudentCourse> studentCourseList = new ArrayList<>();
+    studentCourseList.add(studentCourseJava);
+    studentCourseList.add(studentCourseAWS);
     Mockito.when(repository.searchStudentCourseList()).thenReturn(studentCourseList);
 
-    sut.searchStudentList();
+    StudentDetail studentDetail1 = new StudentDetail();
+    studentDetail1.setStudent(student1);
+    List<StudentCourse> studentCourseList1 = new ArrayList<>();
+    studentCourseList1.add(studentCourseJava);
+    studentDetail1.setStudentsCourses(studentCourseList1);
 
+    StudentDetail studentDetail2 = new StudentDetail();
+    studentDetail2.setStudent(student2);
+    List<StudentCourse> studentCourseList2 = new ArrayList<>();
+    studentCourseList2.add(studentCourseAWS);
+    studentDetail2.setStudentsCourses(studentCourseList2);
+
+    List<StudentDetail> studentDetailList = new ArrayList<>();
+    studentDetailList.add(studentDetail1);
+    studentDetailList.add(studentDetail2);
+
+    // 実行
+    List<StudentDetail> expected = studentDetailList;
+    List<StudentDetail> actual = sut.searchStudentList();
+    Assertions.assertEquals(expected, actual);
+
+    // 検証
     Mockito.verify(repository, Mockito.times(1)).searchStudentList();
     Mockito.verify(repository, Mockito.times(1)).searchStudentCourseList();
     Mockito.verify(converter, Mockito.times(1))
