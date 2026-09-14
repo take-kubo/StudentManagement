c549fb9bf9736974d700e832390ce2d9c1c762b6
take-kubo <takubokawa@gmail.com>
2026-09-15 06:23:01 +0900

    [add]受講生詳細一覧検索のテストの実装
    ・コントローラのgetStudentList()のテストを実装しました
    ・エンポイントにアクセスしたときにステータスコード「OK」が変えることをテストしました
    ・サービスのsearchStudentListが１回呼ばれていることを検証しました

---
+++ src/test/java/raisetech/StudentManagement/controller/StudentControllerTest.java
@@ -0,0 +1,31 @@
+package raisetech.StudentManagement.controller;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import raisetech.StudentManagement.service.StudentService;
+
+@WebMvcTest(StudentController.class)
+class StudentControllerTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @MockitoBean
+  private StudentService service;
+
+  @Test
+  void 受講生詳細の一覧検索が実行できてからのリストが返ってくること() throws Exception {
+    mockMvc.perform(get("/students"))
+        .andExpect(status().isOk());
+
+    verify(service, times(1)).searchStudentList();
+  }
+}
